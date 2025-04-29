67e4ffa271f89b7ec1f8089042c92039de36969d
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-04-29 14:05:06 -0400

    more code

--- CombiApp/EmployeeTableViewCell.swift
+++ CombiApp/EmployeeTableViewCell.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class EmployeeTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var fullNameLabel: UILabel!
+    
+    @IBOutlet weak var titleLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
