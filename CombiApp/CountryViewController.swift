790f74d42af36d4827ee10748b6927a231354889
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-04-29 14:32:38 -0400

    updates

--- CombiApp/CountryViewController.swift
+++ CombiApp/CountryViewController.swift
@@ -7,26 +7,71 @@
 
 import UIKit
 
-class CountryViewController: UIViewController {
-
+class CountryViewController: UIViewController, UITableViewDataSource {
+    var countries: [Country] = []
     @IBOutlet weak var countryTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        countries = readCountriesJson()
+        countryTable.dataSource = self
     }
     
+    /// Read the contents of a `countries.json` file. If there is any failure, this function will fail and take
+    /// the app with it.
+    func readCountriesJson() -> [Country] {
+        guard
+            let url = Bundle.main.url(
+                forResource: "countries",
+                withExtension: "json"
+            )
+        else {
+            print("File not found")
+            abort()
+        }
 
-    /*
-    // MARK: - Navigation
+        guard let data = try? Data(contentsOf: url) else {
+            print("Cannot read file")
+            abort()
+        }
+        let jsonDecorder = JSONDecoder()
+        do {
+            let countries = try jsonDecorder.decode([Country].self, from: data)
+            return countries
+        } catch {
+            print("Error parsing json: \(error)")
+            abort()
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
+
+}
+
+extension CountryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =
+            tableView.dequeueReusableCell(
+                withIdentifier: "countryCell",
+                for: indexPath
+            ) as? CountryTableViewCell
+        cell?.countryCapitalLabel.text = countries[indexPath.row].capital
+        cell?.countryNameLabel.text = countries[indexPath.row].name
+        cell?.countryCodeLabel.text = countries[indexPath.row].code
+        cell?.countryFlagLabel.text = countries[indexPath.row].flag
+        cell?.countryRegionLabel.text = countries[indexPath.row].region
+        cell?.languageNameLabel.text = countries[indexPath.row].language.name
+        cell?.languageCodeLabel.text =
+            countries[indexPath.row].language.code ?? "N/A"
+        cell?.currencyNameLabel.text = countries[indexPath.row].currency.name
+        cell?.currencyCodeLabel.text = countries[indexPath.row].currency.code
+        cell?.currencySymbolLabel.text =
+            countries[indexPath.row].currency.symbol ?? "N/A"
+        return cell ?? UITableViewCell()
+    }
 }
 
 
